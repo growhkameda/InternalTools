a5929fa6fa3d86ea7c0bd1a1cf54b403e7240cd2
syo-k <nazoo4096@gmail.com>
2024-11-09 19:16:04 +0900

    テスト

--- AllMemberView/Back/src/main/java/com/example/internaltools/repository/UserRepository.java
+++ AllMemberView/Back/src/main/java/com/example/internaltools/repository/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.internaltools.entity.UserEntity;
 
+
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 }
