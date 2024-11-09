e16392241983a6b4817f236aa4fc807a22145307
syo-k <nazoo4096@gmail.com>
2024-11-09 17:31:51 +0900

    試し

--- AllMemberView/Back/src/main/java/com/example/internaltools/repository/UserRepository.java
+++ AllMemberView/Back/src/main/java/com/example/internaltools/repository/UserRepository.java
@@ -8,6 +8,7 @@ import com.example.internaltools.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     
+	
     // 部署 ID に基づいてユーザー一覧を取得する
 	List<UserEntity> findByDepartmentName(String departmentName);
 }
